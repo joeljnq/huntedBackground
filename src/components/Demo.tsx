f11c1f2372fd2fa139971c601599fef6c305cc1d
joel <joelnqwork@gmail.com>
2024-10-23 13:51:15 +0200

    fix(demo): center the comparison slider

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -3,6 +3,9 @@ import originalDemo from '../assets/images/originalDemo.webp'
 import { ComparisonSlider } from "./ComparisonSlider";
 export const Demo = () => {
     return(
+        <div className='container bg-gradient-to-b max-w-full from-black to-gray-900/90 min-h-screen '> 
+
         <ComparisonSlider bottomImage={{src:originalDemo, alt:'original'}} topImage={{src:transformedDemo,alt:'transformed'}} />
+        </div>
     )
 } 
